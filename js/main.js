e89eeb111d1b126d74b12f42a3f71a4b97f9824c
Michael Cockle <michaelcockle@Michaels-MacBook-Pro.local>
2016-03-27 23:52:33 +0100

    amend form

---
+++ js/main.js
@@ -0,0 +1,28 @@
+console.log('main.js');
+
+// var nme = 'http://' + 'formspree.io/' + 'michaelantoney' + '@' + 'gmail' + '.' + 'com';
+
+// $('body').on('click', '#submitContactForm', function(e) {
+//     e.preventDefault();
+//     console.log('form submited');
+//     contactForm();
+// });
+
+// function contactForm() {
+//     $.ajax({
+//         url: nme,
+//         method: 'POST',
+//         data: $('#myform').serialize(),
+//         dataType: 'json',
+//         beforeSend: function() {
+//             console.log("sending message...");
+//         },
+//         success: function(data) {
+//             console.log("success message...");
+//         },
+//         error: function(err) {
+//             console.log("error message...");
+//         }
+//     });
+
+// }
