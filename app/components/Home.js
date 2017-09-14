08374daedda189f572dcea7ff4e5435babd48f3c
Michael mac <m.cockle@9xb.com>
2017-09-14 22:06:29 +0100

    add state

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,25 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ShowCard from './ShowCard'
 import preload from '../../data/data-netflix.json'
-import {
-    BrowserRouter as Router,
-    Route,
-    Link
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 let Home = React.createClass({
-    render: function () {
+    getInitialState() {
+        return {
+            searchTerm: 'Default String **',
+        }
+    },
+
+    handleSearchTermChange(event) {
+        this.setState({ searchTerm: event.target.value })
+    },
+
+    render: function() {
         return (
             <div>
-                <Link to='/topics'>Link to Map</Link> {preload.shows.map((show) => {
-                return (
-                    <ShowCard key={show.imdbID} {...show}/>
-                )
-            })}
+                <header>
+                    <h1>{this.state.searchTerm}</h1>
+                    <input
+                        onChange={this.handleSearchTermChange}
+                        value={this.state.searchTerm}
+                        type="text"
+                        placeholder="Search"
+                    />
+                </header>
+                <p>
+                    <Link to="/topics">Link to Map</Link>
+                </p>
+                {preload.shows.map(show => {
+                    return <ShowCard key={show.imdbID} {...show} />
+                })}
             </div>
         )
-    }
-});
+    },
+})
 
 export default Home
 
