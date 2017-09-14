1504e65d30b57ca6522ce0ac443bb09d4f23f6a9
Michael mac <m.cockle@9xb.com>
2017-09-14 22:29:13 +0100

    add filter to search

--- app/components/Home.js
+++ app/components/Home.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 let Home = React.createClass({
     getInitialState() {
         return {
-            searchTerm: 'Default String **',
+            searchTerm: '',
         }
     },
 
@@ -30,9 +30,22 @@ let Home = React.createClass({
                 <p>
                     <Link to="/topics">Link to Map</Link>
                 </p>
-                {preload.shows.map(show => {
-                    return <ShowCard key={show.imdbID} {...show} />
-                })}
+
+                <div className="show-card-wrap">
+                    {preload.shows
+                        .filter(show => {
+                            return (
+                                `${show.title} ${show.description}`
+                                    .toUpperCase()
+                                    .indexOf(
+                                        this.state.searchTerm.toUpperCase(),
+                                    ) >= 0
+                            )
+                        })
+                        .map(show => {
+                            return <ShowCard key={show.imdbID} {...show} />
+                        })}
+                </div>
             </div>
         )
     },
--- app/components/ShowCard.js
+++ app/components/ShowCard.js
@@ -1,27 +1,30 @@
 import React from 'react'
 
-const {string} = React.PropTypes
+const { string } = React.PropTypes
 
 const ShowCard = React.createClass({
-
     propTypes: {
-            poster:      string,
-            title:       string,
-            year:        string,
-            description: string
+        poster: string,
+        title: string,
+        year: string,
+        description: string,
     },
 
     render() {
-        const {poster, title, year, description} = this.props
+        const { poster, title, year, description } = this.props
         return (
-            <div className='show-card'>
-                <h3>{title}</h3>
-                <h3>{year}</h3>
-                <p>{description}</p>
-                <img src={`/dist/img/posters/${poster}`}/>
+            <div className="show-card">
+                <div className="show-card-img-wrap">
+                    <img src={`/dist/img/posters/${poster}`} />
+                </div>
+                <div className="show-copy-wrap">
+                    <h3>{title}</h3>
+                    <h3>{year}</h3>
+                    <p>{description}</p>
+                </div>
             </div>
         )
-    }
+    },
 })
 
 export default ShowCard
