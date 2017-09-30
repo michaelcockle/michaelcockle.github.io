bba8bfcfd5265d36bd31cbb032a06861f1cb92f0
Michael mac <m.cockle@9xb.com>
2017-09-30 14:15:01 +0100

    source maps not working, experiment

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,61 +1,66 @@
-let path = require('path');
+let path = require('path')
 
-let webpack = require('webpack');
+let webpack = require('webpack')
 
-let HtmlWebpackPlugin = require('html-webpack-plugin');
+let HtmlWebpackPlugin = require('html-webpack-plugin')
 
-let ExtractTextPlugin = require("extract-text-webpack-plugin");
+let ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 let HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
     template: __dirname + '/app/index.html',
     filename: 'index.html',
-    inject:   'body',
-});
+    inject: 'body',
+})
 
 let SASSWebpackPluginConfig = new ExtractTextPlugin({
-    filename:  "main.css",
-    allChunks: true
-});
+    filename: 'main.css',
+    allChunks: true,
+})
 
 module.exports = {
-    entry:     [
+    entry: [
         'react-hot-loader/patch',
         'webpack-dev-server/client?http://localhost:8081',
         'webpack/hot/only-dev-server',
-        './app/index.js'
+        './app/index.js',
     ],
+
     devServer: {
         hot: true,
         inline: true,
         publicPath: '/',
-        historyApiFallback: true
+        historyApiFallback: true,
     },
-    //devtool:   '#inline-source-map',
+
     devtool:   'cheap-eval-source-map',
-    output:    {
-        library:  'Example', // window.Example
-        path:     __dirname + '/dist',
-        filename: "index_bundle.js",
-        publicPath: "/"
+    //devtool: '#inline-source-map',
+    //devtool:   'eval-source-map',
+    //devtool:   'eval',
+    //devtool:   'cheap-module-eval-source-map',
+    //devtool: 'source-map',
+
+    output: {
+        library: 'Example', // window.Example
+        path: __dirname + '/dist',
+        filename: 'index_bundle.js',
+        publicPath: '/',
     },
-    resolve:   {
-        extensions: ['*', '.js', '.jsx', '.css', '.scss', '.json']
-        //extensions: ['*', '.js', '.jsx']
+
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.css', '.scss', '.json'],
     },
-    module:    {
+
+    module: {
         rules: [
             {
-                test: [
-                    /\.js$/,
-                    /\.jsx?$/
-                ],
+                test: [/\.js$/, /\.jsx?$/],
 
                 exclude: /node_modules/,
-                use:     "babel-loader"
+                use: 'babel-loader',
             },
             {
                 test: /\.json$/,
-                use:  'json-loader'
+                use: 'json-loader',
             },
             /*
              * Load inline styles
@@ -65,18 +70,8 @@ module.exports = {
             //    test:    [
             //        /\.scss$/
             //    ],
-            //
             //    exclude: /node_modules/,
             //    loader:  "style-loader!css-loader!autoprefixer-loader!sass-loader"
-            //},
-
-            //{
-            //    test: /\.css$/,
-            //    exclude: /node_modules/,
-            //    use: ExtractTextPlugin.extract({
-            //        fallback: 'style-loader',
-            //        loader: 'css-loader'
-            //    }),
             //},
 
             /*
@@ -84,32 +79,21 @@ module.exports = {
              * */
 
             {
-                test: [
-                    /\.scss$/
-                ],
-                exclude:
-                      /node_modules/,
-                use:
-                      ExtractTextPlugin.extract({
-                          fallback: "style-loader",
-                          //use: ['css-loader', 'sass-loader']
-                          //use: ['style-loader','css-loader','autoprefixer-loader','sass-loader']
-                          //use:  "style-loader!css-loader!autoprefixer-loader!sass-loader"
-                          //use:  "css-loader!autoprefixer-loader!sass-loader"
-                          use:      ['css-loader', 'autoprefixer-loader', 'sass-loader']
-                      })
-
-            }
-
-        ]
+                test: [/\.scss$/],
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'autoprefixer-loader', 'sass-loader'],
+                }),
+            },
+        ],
     },
 
-    //devtool:   'source-map',
     plugins: [
         HTMLWebpackPluginConfig,
         SASSWebpackPluginConfig,
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin()
-    ]
+        new webpack.NamedModulesPlugin(),
+    ],
     //plugins:   [HTMLWebpackPluginConfig]
-};
+}
