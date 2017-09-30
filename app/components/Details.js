f4bc5b577962b103c3f3381f7a8cf6f59229c92a
Michael mac <m.cockle@9xb.com>
2017-09-30 16:52:37 +0100

    convert Details to ES6 Class

--- app/components/Details.js
+++ app/components/Details.js
@@ -1,33 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Header from './Header'
 
+class Details extends Component {
 
-const Details = props => {
-    const { title, description, year, poster, trailer } = props.show
-    return (
-        <div className="details">
+    //props: {
+    //    show: Show,
+    //}
 
-            <Header />
-            <h2>Details</h2>
+    render() {
+        const { title, description, year, poster, trailer } = this.props.show
+        return (
+            <div className="details">
+                <Header />
+                <h2>Details</h2>
 
-            <section>
-                <h1>{title}</h1>
-                <h2>({year})</h2>
-                <p>{description}</p>
-                <div className="img-project-detail">
-                    <img
-                        src={`/dist/img/posters/${poster}`}
-                        alt={`project is ${title} `}
-                    />
-                </div>
-            </section>
+                <section>
+                    <h1>{title}</h1>
+                    <h2>({year})</h2>
+                    <p>{description}</p>
+                    <div className="img-project-detail">
+                        <img
+                            src={`/dist/img/posters/${poster}`}
+                            alt={`project is ${title} `}
+                        />
+                    </div>
+                </section>
 
-            <hr />
-            <pre>
-                <code>{JSON.stringify(props, null, 4)}</code>
-            </pre>
-        </div>
-    )
+                <pre>
+                    <code>{JSON.stringify(this.props, null, 4)}</code>
+                </pre>
+            </div>
+        )
+    }
 }
-
 export default Details
