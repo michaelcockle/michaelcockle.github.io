52b29b23b17d45a358fd4626946daf014f462dea
Michael mac <m.cockle@9xb.com>
2017-11-08 17:03:03 +0000

    styles, tracking

--- app/components/About.js
+++ app/components/About.js
@@ -7,7 +7,6 @@ import Header from './Header'
 class About extends Component {
     constructor(props) {
         super(props)
-
         this.state = {
             infoCopy: 'This is info',
         }
