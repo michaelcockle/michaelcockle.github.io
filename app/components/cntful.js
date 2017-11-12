3462d8eb959a81c3109eb324670767fe9e653bfc
Michael mac <m.cockle@9xb.com>
2017-11-12 22:51:40 +0000

    js data exp

--- app/components/cntful.js
+++ app/components/cntful.js
@@ -12,54 +12,50 @@
 //}
 
 let getData = {};
-const url = "https://cdn.contentful.com/spaces/xq9fh8s1o76v/entries?access_token=f2b7ef5e7ac493e6714f70081a4d02ce892238ef8546d9e2704247e631f338ee";
+//const url   = "https://cdn.contentful.com/spaces/xq9fh8s1o76v/entries?access_token=f2b7ef5e7ac493e6714f70081a4d02ce892238ef8546d9e2704247e631f338ee";
 
-function fetchPosts() {
+function fetchPosts(url) {
+    // Return a new promise.
+    return new Promise(function (resolve, reject) {
+        // Do the usual XHR stuff
+        let request = new XMLHttpRequest();
+        request.open('GET', url);
 
-    function get(url) {
-        // Return a new promise.
-        return new Promise(function(resolve, reject) {
-            // Do the usual XHR stuff
-            let request = new XMLHttpRequest();
-            request.open('GET', url);
+        request.onload = function () {
+            // This is called even on 404 etc so check the status
+            if (request.status === 200) {
+                // Resolve the promise with the response text
+                resolve(request.response);
+            }
+            else {
+                // Otherwise reject with the status text which will hopefully be a meaningful error
+                reject(Error(request.statusText));
+            }
+        };
 
-            request.onload = function() {
-                // This is called even on 404 etc so check the status
-                if (request.status === 200) {
-                    // Resolve the promise with the response text
-                    resolve(request.response);
-                }
-                else {
-                    // Otherwise reject with the status text which will hopefully be a meaningful error
-                    reject(Error(request.statusText));
-                }
-            };
+        // Handle network errors
+        request.onerror = function () {
+            reject(Error("Network Error"));
+        };
 
-            // Handle network errors
-            request.onerror = function() {
-                reject(Error("Network Error"));
-            };
-
-            // Make the request
-            request.send();
-        });
-    }
-
-    get(url).then(function(response) {
-        //console.log("Success!", response);
-        console.log('PROMISE');
-
-        let data = JSON.parse(response);
-        getData.data2 = data;
-
-        console.log(data);
-        console.log(data.includes.Asset["0"].fields);
-        console.log('BLOG = ', data.items["0"].fields.shortDescription);
-
-
-    }, function(error) {
-        console.error("Failed!", error);
+        // Make the request
+        request.send();
     });
 }
+
+//fetchPosts(url).then(function (response) {
+//
+//    console.log('PROMISE');
+//
+//    let data      = JSON.parse(response);
+//    getData.data2 = data;
+//
+//    console.log('data === ', data);
+//    console.log(data.includes.Asset["0"].fields);
+//    console.log('BLOG = ', data.items["0"].fields.shortDescription);
+//
+//}, function (error) {
+//    console.error("Failed!", error);
+//});
 
 export {fetchPosts, getData}
--- app/index.js
+++ app/index.js
@@ -9,36 +9,58 @@ import {
     Match,
 } from 'react-router-dom'
 
-import {fetchPosts, getData } from './components/cntful';
+import {fetchPosts, getData} from './components/cntful';
+
 fetchPosts();
 
-
-console.log('------- ****** -------');
-
-console.log('fetchPosts = ', fetchPosts.value);
-
-console.log('getData ===== ', getData);
+const Test = () => {
 
 
-console.log('------- ****** -------');
-
-
-//getData.forEach(function(item){
-//    console.log(item);
-//});
-
-console.log('------- ¢¢¢¢¢¢¢ -------');
-
-
-const renderApp = () => {
-    render( <Router><App/></Router>, document.getElementById('app'))
+    return (
+        <div className="content-sub__wrap">
+            <h2>Hello World*</h2>
+        </div>
+    )
 }
 
+const url = "https://cdn.contentful.com/spaces/xq9fh8s1o76v/entries?access_token=f2b7ef5e7ac493e6714f70081a4d02ce892238ef8546d9e2704247e631f338ee";
+
+fetchPosts(url).then(function (response) {
+
+    let data = JSON.parse(response);
+
+    console.log('data === ', data);
+    console.log(data.includes.Asset["0"].fields);
+    console.log('BLOG = ', data.items["0"].fields.shortDescription);
+
+    data.items.forEach(function (item) {
+        console.log('item == ', item);
+        let elem = item.fields.shortDescription;
+        console.log('ELEM = ', elem);
+        //.fields.title
+        //item.fields.forEach(function (elem) {
+        //    console.log('elem = ', elem);
+        //})
+    });
+    let passvar = "pass var";
+    renderTest(passvar)
+
+}, function (error) {
+    console.error("Failed!", error);
+});
+
+const renderApp = () => {
+    render(<Router><App/></Router>, document.getElementById('app'))
+}
 renderApp()
+
+const renderTest = ({passvar}) => {
+    render(<Test/>, document.getElementById('test'))
+}
 
 if (module.hot) {
     module.hot.accept('./components/App', () => {
-       renderApp();
+        renderApp();
     });
 }
 
