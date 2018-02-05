ae656e25aff6b97772e3a475bdca6bbab9ab6180
Michael mac <m.cockle@9xb.com>
2018-02-05 21:27:48 +0000

    mess with data

--- app/index.js
+++ app/index.js
@@ -7,7 +7,6 @@ import {fetchPosts, getData} from './components/cntful';
 
 fetchPosts();
 
-
 const url =
           'https://cdn.contentful.com/spaces/xq9fh8s1o76v/entries?access_token=f2b7ef5e7ac493e6714f70081a4d02ce892238ef8546d9e2704247e631f338ee';
 
@@ -15,31 +14,69 @@ fetchPosts(url).then(
     function (response) {
         let data = JSON.parse(response);
 
-        //console.clear();
-
+        console.clear();
         console.log(data);
+
+        //let dataJson = JSON.stringify(data)
+        //console.log(dataJson);
+
+        //let dataArray = Array.from(data.items)
+        //console.log(dataArray);
+        //
+
 
         let dataItems = data.items;
 
-        console.log('-------');
+        //console.log('-------');
         //console.table(dataItems);
         console.log(dataItems);
-        console.log(typeof dataItems);
-        //console.log('Array = ' , Array.isArray(dataItems));
-        console.log('-------');
+        //console.log('Is DataItems an Array = ', Array.isArray(dataItems)); // true
 
         for (var prop of dataItems) {
-            //console.log('prop = ', prop);
+            //console.log('typeof prop = ', typeof prop);
+            //console.log('Is prop an Array = ', Array.isArray(prop)); // false
+
+            console.log('prop = ', prop);
 
             for (const item in prop) {
+
                 const value = prop[item];
+
                 //console.log('Item **** = ', item);
                 //console.log('ITEM = ', item, ' : value = ', value);
-                console.log('---***----');
+                //console.log('Value **** = ', value);
+
+                console.log('%c prop.sys.contentType = ', 'color: yellow;', prop.sys.contentType);
+                //console.log('%c prop.sys.contentType = ', 'color: yellow;', prop.sys.contentType.sys.id);
+                console.log('______________-');
+                console.log('%c prop.id = ', 'color: green;', prop.sys.id);
+                console.log('%c prop.createAt= ', 'color: purple;', prop.sys.createdAt);
+                console.log('%c prop.updatedAt = ', 'color: purple;', prop.sys.updatedAt);
+
+                if (prop.fields.name !== undefined) {
+                    console.log('%c prop.fields.name = ', 'color: hotpink; font-size: 14px;', prop.fields.name);
+                }
+                if (prop.fields.description !== undefined) {
+                    console.log('%c prop.fields.description = ', 'color: brown; font-size: 14px;', prop.fields.description);
+                }
+                if (prop.fields.title !== undefined) {
+                    console.log('%c prop.fields.title = ', 'color: hotpink; font-size: 14px;', prop.fields.title);
+                }
+
+                if (prop.fields.biography !== undefined) {
+                    console.log('%c prop.biography = ', 'color: blue; font-size: 14px;', prop.fields.biography);
+                }
+
+                if (prop.fields.tags !== undefined) {
+                    console.log('%c prop.biography = ', 'color: yellow; font-size: 14px;', prop.fields.tags);
+                }
+
+                //console.log('Value = ', value);
+                //console.log('value.name = ', value[0]);
+
             }
 
             // how get index ?
-
 
             // ["0"].fields
 
@@ -73,6 +110,7 @@ fetchPosts(url).then(
 
             //console.log(item);
 
+            //console.table(item.sys);
             //console.table(item.fields);
 
             //let fields = item.fields;
@@ -100,8 +138,6 @@ fetchPosts(url).then(
     },
 );
 
-
-
 //const renderApp = () => {
 //    render( <Router><App/></Router>, document.getElementById('app'))
 //}
@@ -114,17 +150,12 @@ fetchPosts(url).then(
 //    });
 //}
 
-
-if(module.hot) {
+if (module.hot) {
     module.hot.accept();
     console.log("index.js HMR");
     const NewApp = require('./components/App').default;
     render(<Router><App/></Router>, document.getElementById('app'));
 }
-
-
-
-
 
 // if (module.hot) module.hot.accept()
 /* <pre className="app--data">{JSON.stringify(this.state.data, null, 2)}</pre> */
